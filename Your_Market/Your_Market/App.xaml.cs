c7e7d3af70dfbf25205b5b86c36774aa2023b108
Fantomas077 <nheltonn@gmail.com>
2022-09-08 16:21:40 +0200

    my first commit

---
+++ Your_Market/Your_Market/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Your_Market.Views;
+
+namespace Your_Market
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var navigationPage= new NavigationPage(new Home());
+
+            navigationPage.BarBackgroundColor = Color.FromHex("#E2AA18");
+            MainPage = navigationPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
